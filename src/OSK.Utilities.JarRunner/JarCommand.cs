c0195ea4aaeac51eba58b9485e37e8f14bcdba13
M <mmmasent@gmail.com>
2024-06-20 22:00:28 -0400

    Add initial project (OSK-1)
    Motivation
    ----
    Want a simple library for executing a jar command that can be shared across projects
    
    Modifications
    ----
    * Added basic JarCommand/ProcessExtensions to handle executing a jar file from C#
    * Added github workflows
    
    Result
    ----
    A jar file can now be executed from C# using a simple library

---
+++ src/OSK.Utilities.JarRunner/JarCommand.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OSK.Utilities.JarRunner
+{
+    public abstract class JarCommand<TResult>
+    {
+        public async Task<TResult> ExecuteAsync(string jarFilePath, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                await PrepareAsync(cancellationToken);
+                using var process = new Process()
+                {
+                    EnableRaisingEvents = true,
+                    StartInfo = new ProcessStartInfo("java")
+                    {
+                        RedirectStandardInput = true,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        WindowStyle = ProcessWindowStyle.Hidden,
+                        UseShellExecute = false,
+                        CreateNoWindow = true,
+                        Arguments = $"-jar \"{jarFilePath}\" {string.Join(" ", GetArgumentList())}"
+                    },
+                };
+
+                process.ErrorDataReceived += (sender, args) => throw new InvalidOperationException(
+                    $"An error executing the jar command of type {GetType().FullName}: {args.Data}");
+
+                await process.RunAsync();
+                var error = await process.GetErrorBytesAsync();
+                if (error.Any())
+                {
+                    var message = Encoding.UTF8.GetString(error);
+                    throw new InvalidOperationException($"Error occurred using the jar command of type {GetType().FullName}. Error {message}");
+                }
+
+                return await GetResultAsync(process, cancellationToken);
+            }
+            finally
+            {
+                CleanUp();
+            }
+        }
+
+        protected abstract ValueTask<TResult> GetResultAsync(Process process, CancellationToken cancellationToken);
+
+        protected virtual ValueTask PrepareAsync(CancellationToken cancellationToken = default)
+        {
+            return new ValueTask();
+        }
+
+        protected virtual IEnumerable<string> GetArgumentList()
+        {
+            return Enumerable.Empty<string>();
+        }
+
+        protected virtual void CleanUp()
+        {
+        }
+    }
+}
