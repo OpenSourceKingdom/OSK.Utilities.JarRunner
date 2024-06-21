36f57691ec67ad349b888b4188fdd9f2fe3abbcc
BlankDev117 <mmmasent@gmail.com>
2024-06-20 22:03:45 -0400

    Add initial project (OSK-1) (#2)
    Motivation
    ----
    Want a simple library for executing a jar command that can be shared
    across projects
    
    Modifications
    ----
    * Added basic JarCommand/ProcessExtensions to handle executing a jar
    file from C#
    * Added github workflows
    
    Result
    ----
    A jar file can now be executed from C# using a simple library
    
    Fixes #1

---
+++ src/OSK.Utilities.JarRunner/ProcessExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace OSK.Utilities.JarRunner
+{
+    public static class ProcessExtensions
+    {
+        public static Task RunAsync(this Process process)
+        {
+            var tcs = new TaskCompletionSource<int>();
+
+            process.EnableRaisingEvents = true;
+            process.Exited += (sender, args) =>
+            {
+                tcs.SetResult(process.ExitCode);
+            };
+
+            process.Start();
+
+            return tcs.Task;
+        }
+
+        public static async Task<byte[]> GetErrorBytesAsync(this Process process)
+        {
+            await using var memoryStream = new MemoryStream();
+            await process.StandardError.BaseStream.CopyToAsync(memoryStream);
+
+            return memoryStream.ToArray();
+        }
+    }
+}
